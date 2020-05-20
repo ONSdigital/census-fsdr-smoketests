abf3112921215901051648d38a5e7af643904fbe
KieranWardle <kieranwardle95@gmail.com>
2020-05-20 15:32:54 +0100

    changed url

--- src/main/java/uk/gov/ons/census/fsdr/smoketests/AdeccoHelper.java
+++ src/main/java/uk/gov/ons/census/fsdr/smoketests/AdeccoHelper.java
@@ -20,7 +20,7 @@ import java.io.IOException;
 @Component
 public class AdeccoHelper {
 
-  @Value("${security.oauth2.resource.baseurl}")
+  @Value("${security.oauth2.resource.url}")
   private String ADECCO_URL;
 
   @Value("${security.oauth2.client.accessTokenUri}")
